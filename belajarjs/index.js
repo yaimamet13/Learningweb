3417bd53d719052c39037d4373c00bebf820f21e
Rakhmat Azhari <yai.mamet13@gmail.com>
2017-10-23 13:24:23 +0700

    belajar web dsign

---
+++ belajarjs/index.js
@@ -0,0 +1,18 @@
+
+
+alert("Selamat datang di website saya");
+
+var nama = prompt('Masukan Nama Anda');
+var alamat = prompt('Dimana Alamat Rumah Anda?');
+var sekolah = prompt('Sekolahnya diman?')
+
+alert("Nama Aku adalah "+nama+', alamat rumah di '+alamat+' sekolah aku di '+sekolah);
+
+var angka1 =prompt('Masukan angka pertama');
+var angka2 = prompt('Masukan angka kedua');
+var
+
+hasil = angka1 * angka2;
+hasil1 = angka1 / angka2;
+hasil2 = angka1 + angka2;
+hasil3 = angka1 - angka2;
